54e136d383252b50b1f78510e6e629ff15443190
Douglas <doug.lb07@gmail.com>
2020-01-10 12:33:29 -0300

    Validating requests with JOI

--- 09-api/src/routes/heroRoutes.js
+++ 09-api/src/routes/heroRoutes.js
@@ -1,4 +1,5 @@
 const BaseRoute = require('./base/baseRoute')
+const Joi = require('joi')
 
 class HeroRoutes extends BaseRoute {
     constructor(db) {
@@ -10,20 +11,36 @@ class HeroRoutes extends BaseRoute {
         return {
             path: '/herois',
             method: 'GET',
+            config: {
+                validate: {
+                    // payload -> body
+                    // headers -> header
+                    // params -> na URL :id
+                    // query -> ?skip=10&limit=100
+                    failAction: (request, headers, erro) => {
+                        throw erro
+                    },
+                    query: {
+                        skip: Joi.number().integer().default(0),
+                        limit: Joi.number().integer().default(10),
+                        nome: Joi.string().min(3).max(100)
+                    }
+                }
+            },
             handler: (request, headers) => {
                 try {
-                    const { skip, limit, nome } = request.query
+                    const { 
+                        skip, 
+                        limit, 
+                        nome 
+                    } = request.query
 
-                    let query = {}
-                    if (nome) {
-                        query.nome = nome
-                    }
-                    if (isNaN(skip))
-                        throw Error('O tipo do skip é incorreto')
-                    if (isNaN(limit))
-                        throw Error('O tipo do limit é incorreto')
+                    const query =  { 
+                        nome: {$regex: `.*${nome}*.`
+                        } 
+                    } 
 
-                    return this.db.read(query, parseInt(skip), parseInt(limit))
+                    return this.db.read(nome ? query : {}, skip, limit)
 
                 } catch (error) {
                     console.log('DEU RUIM!', error)
--- 09-api/src/tests/apiHeroes.test.js
+++ 09-api/src/tests/apiHeroes.test.js
@@ -4,17 +4,17 @@ let app = {}
 
 describe('Suite de testes da API Heroes', function () {
     this.beforeAll(async () => {
-       app = await api 
+        app = await api
     })
 
     it('Listar /herois', async () => {
         const result = await app.inject({
             method: 'GET',
-            url:'/herois?skip=0&limit=10'
+            url: '/herois?skip=0&limit=10'
         })
         const dados = JSON.parse(result.payload)
         const statusCode = result.statusCode
-        
+
         assert.deepEqual(statusCode, 200)
         assert.ok(Array.isArray(dados))
     })
@@ -38,7 +38,17 @@ describe('Suite de testes da API Heroes', function () {
             method: 'GET',
             url: `/herois?skip=0&limit=${TAMANHO_LIMITE}`
         })
-        assert.deepEqual(result.payload, 'Erro interno no servidor')
+        const errorResult = {
+            "statusCode": 400,
+            "error": "Bad Request",
+            "message": "child \"limit\" fails because [\"limit\" must be a number]",
+            "validation": {
+                "source": "query",
+                "keys": ["limit"]
+            }
+        }
+        assert.deepEqual(result.statusCode, 400)
+        assert.deepEqual(result.payload, JSON.stringify(errorResult))
     })
 
     it('Listar /herois - deve filtrar um item', async () => {
