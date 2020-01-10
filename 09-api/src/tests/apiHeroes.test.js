e499825fa51c02ef0a4cc60b207e1a785ffd5dd3
Douglas <doug.lb07@gmail.com>
2020-01-10 12:06:48 -0300

    Listing items using GET

--- 09-api/src/tests/apiHeroes.test.js
+++ 09-api/src/tests/apiHeroes.test.js
@@ -10,12 +10,46 @@ describe('Suite de testes da API Heroes', function () {
     it('Listar /herois', async () => {
         const result = await app.inject({
             method: 'GET',
-            url:'/herois'
+            url:'/herois?skip=0&limit=10'
         })
         const dados = JSON.parse(result.payload)
         const statusCode = result.statusCode
         
         assert.deepEqual(statusCode, 200)
         assert.ok(Array.isArray(dados))
+    })
+
+    it('Listar /herois - deve retornar somente 3 registros', async () => {
+        const TAMANHO_LIMITE = 3
+        const result = await app.inject({
+            method: 'GET',
+            url: `/herois?skip=0&limit=${TAMANHO_LIMITE}`
+        })
+
+        const dados = JSON.parse(result.payload)
+        const statusCode = result.statusCode
+        assert.deepEqual(statusCode, 200)
+        assert.ok(dados.length === TAMANHO_LIMITE)
+    })
+
+    it('Listar /herois - deve retornar um erro com limit incorreto', async () => {
+        const TAMANHO_LIMITE = 'AEEE'
+        const result = await app.inject({
+            method: 'GET',
+            url: `/herois?skip=0&limit=${TAMANHO_LIMITE}`
+        })
+        assert.deepEqual(result.payload, 'Erro interno no servidor')
+    })
+
+    it('Listar /herois - deve filtrar um item', async () => {
+        const NAME = 'Homem Aranha-1578581997088'
+        const result = await app.inject({
+            method: 'GET',
+            url: `/herois?skip=0&limit=1000&nome=${NAME}`
+        })
+        const dados = JSON.parse(result.payload)
+        const statusCode = result.statusCode
+        assert.deepEqual(statusCode, 200)
+        assert.ok(dados[0].nome === NAME)
     })
 })
